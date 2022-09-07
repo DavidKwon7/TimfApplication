58bd0afcfd4916a28aa2e6641794327a395f6793
DavidKwon7 <khjunejune10@gmail.com>
2022-09-07 20:21:34 +0900

    create 'Activity'

---
+++ app/src/main/java/com/example/timfapplication/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.timfapplication.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timfapplication.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
