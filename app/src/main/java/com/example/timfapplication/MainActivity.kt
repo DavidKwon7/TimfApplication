73fee7ad839245bf62f709a22b5c764221c1895c
DavidKwon7 <khjunejune10@gmail.com>
2022-09-07 19:13:24 +0900

    add 'bottomNav'

--- app/src/main/java/com/example/timfapplication/MainActivity.kt
+++ app/src/main/java/com/example/timfapplication/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.timfapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val jobFragment = JobFragment()
+        val calculateFragment = CalculateFragment()
+        val communityFragment = CommunityFragment()
+        val reviewFragment = ReviewFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_homeFragment -> makeCurrentFragment(homeFragment)
+                R.id.item_jobFragment -> makeCurrentFragment(jobFragment)
+                R.id.item_calculateFragment -> makeCurrentFragment(calculateFragment)
+                R.id.item_communityFragment -> makeCurrentFragment(communityFragment)
+                R.id.item_reviewFragment -> makeCurrentFragment(reviewFragment)
+            }
+            true
+        }
     }
+
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.homeFragment, fragment)
+            commit()
+        }
+
 }
