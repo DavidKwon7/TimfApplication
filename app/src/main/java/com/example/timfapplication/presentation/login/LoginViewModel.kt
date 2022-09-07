53ebdd9ea525f08d495676e563f7253a562e3655
DavidKwon7 <khjunejune10@gmail.com>
2022-09-07 20:17:04 +0900

    add 'LoginVM'

---
+++ app/src/main/java/com/example/timfapplication/presentation/login/LoginViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.timfapplication.presentation.login
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.timfapplication.domain.entity.login.RequestLoginModel
+import com.example.timfapplication.domain.entity.login.ResponseLoginModel
+import com.example.timfapplication.domain.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Response
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    val TAG = "vmTest"
+
+    fun postLogin(requestLoginModel: RequestLoginModel) = viewModelScope.launch(Dispatchers.IO) {
+        repository.postLogin(requestLoginModel).enqueue(object : retrofit2.Callback<ResponseLoginModel> {
+            override fun onResponse(
+                call: Call<ResponseLoginModel>,
+                response: Response<ResponseLoginModel>
+            ) {
+                Timber.tag(TAG).d("res")
+                Timber.tag(TAG).d("onResponse: %s", response.body())
+            }
+
+            override fun onFailure(call: Call<ResponseLoginModel>, t: Throwable) {
+                Timber.tag(TAG).d("error")
+                t.printStackTrace()
+            }
+        })
+    }
+}
